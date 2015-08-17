b2ea8ec46574c341477182cfb43ecc78464d70d6
Chad Baron <chadwick37@gmail.com>
2015-08-16 20:05:01 -0700

    update config with namespace

--- herbert.config.php
+++ herbert.config.php
@@ -61,32 +61,32 @@ return [
      * The routes to auto-load.
      */
     'routes' => [
-        'MyPlugin' => __DIR__ . '/app/routes.php'
+        'Qb2Wp' => __DIR__ . '/app/routes.php'
     ],
 
     /**
      * The panels to auto-load.
      */
     'panels' => [
-        'MyPlugin' => __DIR__ . '/app/panels.php'
+        'Qb2Wp' => __DIR__ . '/app/panels.php'
     ],
 
     /**
      * The APIs to auto-load.
      */
     'apis' => [
-        'MyPlugin' => __DIR__ . '/app/api.php'
+        'Qb2Wp' => __DIR__ . '/app/api.php'
     ],
 
     /**
      * The view paths to register.
      *
-     * E.G: 'MyPlugin' => __DIR__ . '/views'
-     * can be referenced via @MyPlugin/
+     * E.G: 'Qb2Wp' => __DIR__ . '/views'
+     * can be referenced via @Qb2Wp/
      * when rendering a view in twig.
      */
     'views' => [
-        'MyPlugin' => __DIR__ . '/resources/views'
+        'Qb2Wp' => __DIR__ . '/resources/views'
     ],
 
     /**
